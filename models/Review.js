fa3cd3d3682b257682083aafc77b365fd691feb2
Duran11 <antonio11duran@gmail.com>
2022-11-21 20:37:59 -0500

    models and db database named

---
+++ models/Review.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    service: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    taste: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quality: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    restaurant_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'restaurant',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,32 @@
 const User = require('./User');
+const Restaurant = require('./Restaurant');
+const Review = require('./Review');
 
-module.exports = { User };
+User.hasMany(Review, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Review.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Restaurant, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Restaurant.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Review.hasOne(Restaurant, {
+  foreignKey: 'review_id',
+  onDelete: 'CASCADE',
+});
+
+Restaurant.belongsTo(Review, {
+  foreignKey: 'review_id',
+});
+
+module.exports = { User, Restaurant, Review };
