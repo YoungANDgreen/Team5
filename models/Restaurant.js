20570973f1cd3c58619189bfbf7c4f642ea693ba
Duran11 <antonio11duran@gmail.com>
2022-11-23 21:15:07 -0500

    added user_id to restuarant model

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -18,6 +18,13 @@ Restaurant.init(
     address: {
       type: DataTypes.STRING,
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
