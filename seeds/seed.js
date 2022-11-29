a5dfa5372dd7160a0dbce1e142d93b651f25e917
Mtomchik <matthew.tomchik@yahoo.com>
2022-11-28 21:07:28 -0700

    hadlebars ect

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Review, Restaurant } = require('../models');
 
 const userData = require('./userData.json');
+const reviewData = require('./reviewData.json');
+const restaurantData = require('./restaurantData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -9,7 +11,17 @@ const seedDatabase = async () => {
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
-  });
+  });  
+  for (const restaurant of restaurantData) {
+    await Restaurant.create({
+      ...restaurant,
+    })
+  };
+  for (const review of reviewData) {
+      await Review.create({
+        ...review,
+      });
+  }
 
   process.exit(0);
 };
