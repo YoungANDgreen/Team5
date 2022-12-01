224a08a91cad539134f08f3d29e09f10023f7f61
Duran11 <antonio11duran@gmail.com>
2022-12-01 10:56:29 -0700

    form for adding new review works

--- public/js/newreview.js
+++ public/js/newreview.js
@@ -1,4 +1,4 @@
-const newFormHandler = async (event) => {
+const newResFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('#review-title').value.trim();
@@ -10,5 +10,28 @@ const newFormHandler = async (event) => {
   const quality = document.querySelector('#review-quality').value.trim();
   const restaurant = document.querySelector('#restaurantFilter').value.trim();
 
+  if (title && description && service && taste && quality && restaurant) {
+    const response = await fetch('/api/reviews', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        description,
+        service,
+        taste,
+        quality,
+        restaurant_id: restaurant,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
+
+document
+  .querySelector('.new-review-form')
+  .addEventListener('submit', newResFormHandler);
