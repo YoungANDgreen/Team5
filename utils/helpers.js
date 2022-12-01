d4d97ee26946479f2de8e29ad31cea27a7aed003
Duran11 <antonio11duran@gmail.com>
2022-12-01 12:54:32 -0700

    new npm package added

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,14 @@
+var oneLinerJoke = require('one-liner-joke');
+
+var getRandomJoke = oneLinerJoke.getRandomJokeWithTag('food').body;
+
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
+  },
+
+  random_joke: () => {
+    return getRandomJoke;
   },
 };
